87681856ca72614d2a99478ecbd951c4ad45d6fd
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-02-22 21:26:33 +0530

    [pushpak singhal]Add. UC-5 Unit Conversion

---
+++ QuantityMeasurementApp/QuantityMeasurementApp.App/QuantityMenu.cs
@@ -0,0 +1,147 @@
+﻿using QuantityMeasurementApp.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using static QuantityMeasurementApp.Core.Entity.Length;
+
+namespace QuantityMeasurementApp.App
+{
+    public sealed class QuantityMenu
+    {
+        //creating a menu class to showcase our work properly
+        public void Menu()
+        {
+            bool flag = true;
+            Console.WriteLine("WELCOME TO QUANTITY MANAGENMENT APP\n");
+            while (flag)
+            {
+
+                Console.WriteLine("1.Feet Measurement Equality");
+                Console.WriteLine("2.Inches Measurement Equality");
+                Console.WriteLine("3.Generic Measurement Equality");
+                Console.WriteLine("4.Unit-To-Unit Conversion");
+                Console.WriteLine("5.Exit");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        isEqualFeet();
+                        break;
+                    case 2:
+                        isEqualInches();
+                        break;
+                    case 3:
+                        isEqualLength();
+                        break;
+                    case 4:
+                        ConvertToUnit();
+                        break;
+                    case 5:
+                        flag = false;
+                        Console.WriteLine("Thanks for visiting");
+                        break;
+                    default:
+                        Console.Error.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+        }
+        // making a seperate method to keep the menu simple and not over crouded
+        //UC-1
+        // these are not needed anymore
+        private static  void isEqualFeet()
+        {
+            try
+            {
+                Console.Write("Enter first feet value: ");
+                Feet f1 = new Feet(double.Parse(Console.ReadLine()));
+                Console.Write("Enter second feet value: ");
+                Feet f2 = new Feet(double.Parse(Console.ReadLine()));
+
+                bool result = f1.Equals(f2);
+                Console.WriteLine("Result: " + result);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+        //UC-2
+        //These are not needed anymore
+        private static void isEqualInches()
+        {
+            try
+            {
+                Console.Write("Enter first Inch value: ");
+                Inches f1 = new Inches(double.Parse(Console.ReadLine()));
+                Console.Write("Enter second Inch value: ");
+                Inches f2 = new Inches(double.Parse(Console.ReadLine()));
+
+                bool result = f1.Equals(f2);
+                Console.WriteLine("Result: " + result);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+        //UC-3
+        private static void isEqualLength()
+        {
+            try
+            {
+                Console.Write("Enter first value: ");
+                double l1=double.Parse(Console.ReadLine());
+                Console.Write("Enter first unit (Feet/Inch/Yard/Centimeters): ");
+                string u1Text = Console.ReadLine();
+                Console.Write("Enter second value: ");
+                double l2=double.Parse(Console.ReadLine());
+                Console.Write("Enter second unit (Feet/Inch/Yard/Centimeters): ");
+                string u2Text = Console.ReadLine();
+
+                if (!Enum.TryParse(u1Text, ignoreCase: true, out LengthUnit u1) || !Enum.TryParse(u2Text, ignoreCase: true, out LengthUnit u2))
+                {
+                    Console.WriteLine("Invalid Unit. Allowed: Feet, Inch ,Yard ,Centimeters");
+                    return;
+                }
+                var v1 = new Length(l1, u1);
+                var v2 = new Length(l2, u2);
+                Console.WriteLine("Result: " + v1.Equals(v2));
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+        //UC-5
+        private static void ConvertToUnit()
+        {
+            try
+            {
+                Console.Write("Enter value");
+                double l1 = double.Parse(Console.ReadLine());
+                Console.Write("Enter Source Unit");
+                string u1Text = Console.ReadLine();
+                Console.Write("Enter Target Unit");
+                string u2Text = Console.ReadLine();
+
+                if (!Enum.TryParse(u1Text, ignoreCase: true, out LengthUnit u1) || !Enum.TryParse(u2Text, ignoreCase: true, out LengthUnit u2))
+                {
+                    Console.WriteLine("Invalid Unit. Allowed: Feet, Inch ,Yard ,Centimeters");
+                    return;
+                }
+                var v1 = new Length(l1, u1);
+                double l2 = v1.ConvertTo(u2);
+
+                Console.WriteLine("Result: " + l1 + " " + u2Text);
+            }
+            catch
+            {
+                Console.WriteLine("Invaild Input");
+            }
+        }
+
+
+    }
+}
---
+++ QuantityMeasurementApp/QuantityMeasurementApp.Core/Entity/Length.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurementApp.Core.Entity
+{
+    //UC-3 making a generic class for all the length 
+    public sealed class Length
+    {
+        private readonly double value;
+        private readonly LengthUnit unit;
+
+        public enum LengthUnit
+        {
+            FEET,
+            INCH,
+            YARD,
+            CENTIMETERS
+        }
+
+        private static double GetConversionFactorToInches(LengthUnit unit)
+        {
+            return unit switch
+            {
+                LengthUnit.FEET => 12.0,   // 1 feet = 12 inches
+                LengthUnit.INCH => 1.0,  // 1 inch = 1 inch
+                LengthUnit.YARD => 36.0, // 1 yard = 36 inches
+                LengthUnit.CENTIMETERS => 0.393701, // 1 centimeters = 0.393701 inches
+                _ => throw new ArgumentOutOfRangeException(nameof(unit), "This Unit is not yet supported.")
+            };
+        }
+        public Length(double value, LengthUnit unit)
+        {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        public double Value => value;
+        public LengthUnit Unit => unit;
+
+        private double ConvertToBaseUnitInches()
+        {
+            return value * GetConversionFactorToInches(unit);
+        }
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj is null || obj.GetType() != GetType()) return false;
+
+            Length other = (Length)obj;
+            return ConvertToBaseUnitInches().CompareTo(other.ConvertToBaseUnitInches()) == 0;
+        }
+        //UC-5
+        public static double Convert(double value, LengthUnit sourceUnit, LengthUnit targetUnit)
+        {
+            if (double.IsNaN(value) || double.IsInfinity(value))
+                throw new ArgumentException("Value must be a finite number.", nameof(value));
+
+            // enums are value-types, but this validates against invalid enum values (casts)
+            if (!Enum.IsDefined(typeof(LengthUnit), sourceUnit))
+                throw new ArgumentException("Source unit is not supported.", nameof(sourceUnit));
+
+            if (!Enum.IsDefined(typeof(LengthUnit), targetUnit))
+                throw new ArgumentException("Target unit is not supported.", nameof(targetUnit));
+
+            // Base unit = inches
+            double sourceFactor = GetConversionFactorToInches(sourceUnit);
+            double targetFactor = GetConversionFactorToInches(targetUnit);
+
+            // Convert: value -> inches -> target
+            double inInches = value * sourceFactor;
+            double inTarget = inInches / targetFactor;
+
+            return inTarget;
+        }
+        public double ConvertTo(LengthUnit targetUnit)
+        {
+            double convertedValue = Convert(this.value, this.unit, targetUnit);
+            return convertedValue;
+        }
+
+        public override int GetHashCode()
+        {
+            return ConvertToBaseUnitInches().GetHashCode();
+        }
+        public override string ToString()
+        {
+            return "Length(value: "+value+", unit: "+unit+")";
+        }
+
+    }
+}
