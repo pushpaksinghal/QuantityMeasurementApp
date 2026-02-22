8d084c1d6a2ac68c057c7fb60ae1b154ed541a80
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-02-22 22:15:36 +0530

    [pushpak singhal]Add. Uc-7 Target Unit Addtion Conversion

---
+++ QuantityMeasurementApp.Core/Entity/Length.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace QuantityMeasurementApp.Core.Entity
+{
+    //UC-3 making a generic class for all the length 
+    public sealed class Length
+    {
+        private readonly double value;
+        private readonly LengthUnit unit;
+
+        public enum LengthUnit
+        {
+            FEET,
+            INCH,
+            YARD,
+            CENTIMETERS
+        }
+
+        private static double GetConversionFactorToInches(LengthUnit unit)
+        {
+            return unit switch
+            {
+                LengthUnit.FEET => 12.0,   // 1 feet = 12 inches
+                LengthUnit.INCH => 1.0,  // 1 inch = 1 inch
+                LengthUnit.YARD => 36.0, // 1 yard = 36 inches
+                LengthUnit.CENTIMETERS => 0.393701, // 1 centimeters = 0.393701 inches
+                _ => throw new ArgumentOutOfRangeException(nameof(unit), "This Unit is not yet supported.")
+            };
+        }
+        public Length(double value, LengthUnit unit)
+        {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        public double Value => value;
+        public LengthUnit Unit => unit;
+
+        private double ConvertToBaseUnitInches()
+        {
+            return value * GetConversionFactorToInches(unit);
+        }
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj is null || obj.GetType() != GetType()) return false;
+
+            Length other = (Length)obj;
+            return ConvertToBaseUnitInches().CompareTo(other.ConvertToBaseUnitInches()) == 0;
+        }
+        //UC-5
+        public static double Convert(double value, LengthUnit sourceUnit, LengthUnit targetUnit)
+        {
+            if (double.IsNaN(value) || double.IsInfinity(value))
+                throw new ArgumentException("Value must be a finite number.", nameof(value));
+
+            // enums are value-types, but this validates against invalid enum values (casts)
+            if (!Enum.IsDefined(typeof(LengthUnit), sourceUnit))
+                throw new ArgumentException("Source unit is not supported.", nameof(sourceUnit));
+
+            if (!Enum.IsDefined(typeof(LengthUnit), targetUnit))
+                throw new ArgumentException("Target unit is not supported.", nameof(targetUnit));
+
+            // Base unit = inches
+            double sourceFactor = GetConversionFactorToInches(sourceUnit);
+            double targetFactor = GetConversionFactorToInches(targetUnit);
+
+            // Convert: value -> inches -> target
+            double inInches = value * sourceFactor;
+            double inTarget = inInches / targetFactor;
+
+            return inTarget;
+        }
+        public double ConvertTo(LengthUnit targetUnit)
+        {
+            double convertedValue = Convert(this.value, this.unit, targetUnit);
+            return convertedValue;
+        }
+        //UC-6
+        public static Length AddUnit(Length l1,Length l2)
+        {
+            if (double.IsNaN(l1.Value) || double.IsInfinity(l1.Value))
+                throw new ArgumentException("Value must be a finite number.", nameof(l1.Value));
+
+            if (double.IsNaN(l2.Value) || double.IsInfinity(l2.Value))
+                throw new ArgumentException("Value must be a finite number.", nameof(l2.Value));
+
+            LengthUnit target = l1.Unit;
+            LengthUnit source = l2.Unit;
+
+            double l2lengthinl1unit = Convert(l2.Value, source, target);
+            return new Length((l1.Value + l2lengthinl1unit), target);
+        }
+        public Length AddUnitTO(Length l2)
+        {
+            if (l2 is null) throw new ArgumentException("First operand cannot be null.", nameof(l2));
+            Length result = AddUnit(new Length(this.Value, this.Unit), l2);
+            return result;
+        }
+
+        public static Length AddToSpecificUnit(Length l1, Length l2 , LengthUnit u1)
+        {
+            var v1 = l1.AddUnitTO(l2);
+            double result = v1.ConvertTo(u1);
+            return new Length(result, u1);
+        }
+        public Length AddSpicificTo(Length l2, LengthUnit u1)
+        {
+            if (l2 is null) throw new ArgumentException("First operand cannot be null.", nameof(l2));
+            Length result = AddToSpecificUnit(new Length(this.Value, this.Unit), l2,u1);
+            return result;
+        }
+        public override int GetHashCode()
+        {
+            return ConvertToBaseUnitInches().GetHashCode();
+        }
+        public override string ToString()
+        {
+            return "Length(value: "+value+", unit: "+unit+")";
+        }
+
+    }
+}
---
+++ QuantityMeasurementApp.Test/EntityTest/LengthAdditionTargetUnitTests.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using QuantityMeasurementApp.Core.Entity;
+using static QuantityMeasurementApp.Core.Entity.Length;
+
+namespace QuantityMeasurementApp.Test.EntityTest
+{
+    [TestClass]
+    public class LengthUc7AdditionTargetUnitTests
+    {
+        private const double Eps = 1e-6;
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_Feet()
+        {
+            var a = new Length(1.0, LengthUnit.FEET);
+            var b = new Length(12.0, LengthUnit.INCH);
+
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.FEET);
+
+            Assert.AreEqual(LengthUnit.FEET, sum.Unit);
+            Assert.AreEqual(2.0, sum.Value, Eps);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_Inch()
+        {
+            var a = new Length(1.0, LengthUnit.FEET);
+            var b = new Length(12.0, LengthUnit.INCH);
+
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.INCH);
+
+            Assert.AreEqual(LengthUnit.INCH, sum.Unit);
+            Assert.AreEqual(24.0, sum.Value, Eps);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_Yard()
+        {
+            var a = new Length(1.0, LengthUnit.FEET);
+            var b = new Length(12.0, LengthUnit.INCH);
+
+            // 1 ft + 12 in = 24 in = 2 ft = 2/3 yd = 0.666666...
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.YARD);
+
+            Assert.AreEqual(LengthUnit.YARD, sum.Unit);
+            Assert.AreEqual(2.0 / 3.0, sum.Value, 1e-6);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_Centimeters()
+        {
+            var a = new Length(1.0, LengthUnit.INCH);
+            var b = new Length(1.0, LengthUnit.INCH);
+
+            // 2 inches = 5.08 cm (approximately)
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.CENTIMETERS);
+
+            Assert.AreEqual(LengthUnit.CENTIMETERS, sum.Unit);
+            Assert.AreEqual(5.08, sum.Value, 1e-3); // looser due to cm factor rounding
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_SameAsFirstOperand()
+        {
+            var a = new Length(1.0, LengthUnit.YARD);
+            var b = new Length(3.0, LengthUnit.FEET);
+
+            // 1 yd + 3 ft = 1 yd + 1 yd = 2 yd
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.YARD);
+
+            Assert.AreEqual(LengthUnit.YARD, sum.Unit);
+            Assert.AreEqual(2.0, sum.Value, Eps);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_SameAsSecondOperand()
+        {
+            var a = new Length(2.0, LengthUnit.YARD);
+            var b = new Length(3.0, LengthUnit.FEET);
+
+            // 2 yd = 6 ft; +3 ft => 9 ft
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.FEET);
+
+            Assert.AreEqual(LengthUnit.FEET, sum.Unit);
+            Assert.AreEqual(9.0, sum.Value, Eps);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_Commutativity()
+        {
+            var a = new Length(1.0, LengthUnit.FEET);
+            var b = new Length(12.0, LengthUnit.INCH);
+
+            Length sum1 = Length.AddToSpecificUnit(a, b, LengthUnit.YARD);
+            Length sum2 = Length.AddToSpecificUnit(b, a, LengthUnit.YARD);
+
+            Assert.AreEqual(LengthUnit.YARD, sum1.Unit);
+            Assert.AreEqual(LengthUnit.YARD, sum2.Unit);
+            Assert.AreEqual(sum1.Value, sum2.Value, 1e-9);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_WithZero()
+        {
+            var a = new Length(5.0, LengthUnit.FEET);
+            var b = new Length(0.0, LengthUnit.INCH);
+
+            // 5 ft => 5/3 yd = 1.666666...
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.YARD);
+
+            Assert.AreEqual(LengthUnit.YARD, sum.Unit);
+            Assert.AreEqual(5.0 / 3.0, sum.Value, 1e-6);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_NegativeValues()
+        {
+            var a = new Length(5.0, LengthUnit.FEET);
+            var b = new Length(-2.0, LengthUnit.FEET);
+
+            // 3 ft = 36 in
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.INCH);
+
+            Assert.AreEqual(LengthUnit.INCH, sum.Unit);
+            Assert.AreEqual(36.0, sum.Value, Eps);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_LargeToSmallScale()
+        {
+            var a = new Length(1000.0, LengthUnit.FEET);
+            var b = new Length(500.0, LengthUnit.FEET);
+
+            // 1500 ft = 18000 in
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.INCH);
+
+            Assert.AreEqual(LengthUnit.INCH, sum.Unit);
+            Assert.AreEqual(18000.0, sum.Value, Eps);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_SmallToLargeScale()
+        {
+            var a = new Length(12.0, LengthUnit.INCH);
+            var b = new Length(12.0, LengthUnit.INCH);
+
+            // 24 inches = 2 feet = 2/3 yard
+            Length sum = Length.AddToSpecificUnit(a, b, LengthUnit.YARD);
+
+            Assert.AreEqual(LengthUnit.YARD, sum.Unit);
+            Assert.AreEqual(2.0 / 3.0, sum.Value, 1e-6);
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_NullSecondOperand_Throws()
+        {
+            var a = new Length(1.0, LengthUnit.FEET);
+
+            Assert.Throws<ArgumentException>(() =>
+                Length.AddToSpecificUnit(a, null, LengthUnit.FEET)
+            );
+        }
+
+        [TestMethod]
+        public void testAddition_ExplicitTargetUnit_InvalidTargetUnit_Throws()
+        {
+            var a = new Length(1.0, LengthUnit.FEET);
+            var b = new Length(1.0, LengthUnit.FEET);
+
+            LengthUnit badTarget = (LengthUnit)999;
+
+            Assert.Throws<ArgumentException>(() =>
+                Length.AddToSpecificUnit(a, b, badTarget)
+            );
+        }
+    }
+}
